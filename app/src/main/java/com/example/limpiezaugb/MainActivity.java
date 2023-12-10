d46aee6c10e3374cfc523d82affdc09b16fbbdd0
Oscar Velasquez <usis935821@ugb.edu.sv>
2023-12-09 21:49:53 -0600

    hice cambios

--- app/src/main/java/com/example/limpiezaugb/MainActivity.java
+++ app/src/main/java/com/example/limpiezaugb/MainActivity.java
@@ -29,57 +29,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        recyclerview = (RecyclerView)findViewById(R.id.rv);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setReverseLayout(true);
-        layoutManager.setStackFromEnd(true);
-        recyclerview.setLayoutManager(layoutManager);
-
-        FirebaseRecyclerOptions<MainModel> options =
-                new FirebaseRecyclerOptions.Builder<MainModel>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("areas").orderByChild("createdAt"), MainModel.class)
-                        .build();
-
-        mainAdapter = new MainAdapter(options);
-        recyclerview.setAdapter(mainAdapter);
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int itemId = item.getItemId();
-                if (itemId == R.id.navigation_inicio) {
-                    startActivity(new Intent(MainActivity.this, MainActivity.class));
-                    return true;
-                } else if (itemId == R.id.navigation_asignacion) {
-                    startActivity(new Intent(MainActivity.this, AsignacionAreas.class));
-                    return true;
-                } else if (itemId == R.id.navigation_empleados) {
-                    startActivity(new Intent(MainActivity.this, NuevoActivity.class));
-                    return true;
-                } else if (itemId == R.id.navigation_logout) {
-                    // Cerrar Sesión
-                    FirebaseAuth.getInstance().signOut(); // Cierra la sesión actual
-                    startActivity(new Intent(MainActivity.this, Login.class)); // Redirige a la pantalla de inicio de sesión
-                    finish(); // Cierra la actividad actual
-                    return true;
-                }
-                return false;
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mainAdapter.startListening();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mainAdapter.stopListening();
     }
 
 }
